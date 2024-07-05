a3af60ca18d9070e29c922240dd03ddd178d5660
ShengqiangLi <lichengqiang@lichengqiangdeMacBook-Pro.local>
2024-07-05 11:17:26 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
-method='cut'
-lang=zh
-input_file=examples/input.json
-gen_wav_dir=examples/gen_wav
-result_file=examples/result_detail.json
-
 python main.py \
-  --input_file $input_file \
-  --result_file $result_file \
-  --wav_dir $gen_wav_dir \
-  --lang $lang \
-  --method $method \
+  --input_file examples/input.json \
+  --result_file examples/result_detail.json \
+  --wav_dir examples/gen_wav \
+  --lang zh \
+  --method cut \
   --sim_model eres2net \
-  --device cpu &
+  --device 'cuda:0'
 
 
 python average.py \
-  --input_file $result_file \
-  --result_file /Users/lichengqiang/Documents/code/TTS-Evaluation/examples/result_avg.json
+  --input_file examples/result_detail.json \
+  --result_file examples/result_avg.json
 
